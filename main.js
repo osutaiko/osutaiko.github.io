cec13a49012b90f50bacc60c18f2f79f54187dec
Dongheon Han <henongod@kaist.ac.kr>
2024-03-07 20:41:16 +0900

    added tutorial

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
     const loginButton = document.getElementById('login-button');
+    const myTimesButton = document.getElementById('mytimes-button');
+    const helpButton = document.getElementById('help-button');
+    const closeModalButton = document.getElementsByClassName('close')[0];
 
     loginButton.addEventListener('click', () => {
         alert("why");
+    });
+
+    myTimesButton.addEventListener('click', () => {
+        alert("why");
+    });
+
+    helpButton.addEventListener('click', () => {
+        modal.style.display = 'block';
+    });
+
+    closeModalButton.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+    window.addEventListener('click', (event) => {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
     });
 });
